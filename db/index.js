39cc40f85d9c290328b071ac93fcd9abade4e74c
Moisuc Vasile Vlad <moisuc.vlad1@gmail.com222>
2021-01-27 21:57:07 +0200

    Add database connection URL.

--- db/index.js
+++ db/index.js
@@ -2,10 +2,13 @@ const mongoose = require('mongoose');
 
 const connect = async () => {
   try {
-    await mongoose.connect('mongodb://localhost/users', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      `mongodb+srv://root:${process.env.databasePasword}@cluster0.v6uxj.gcp.mongodb.net/<welou>?retryWrites=true&w=majority`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
     console.log('Connected to MongoDB.');
   } catch (error) {
     console.log('Could not connect to MongoDB because:', error);
